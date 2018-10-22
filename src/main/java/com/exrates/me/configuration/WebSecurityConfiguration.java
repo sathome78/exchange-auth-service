c261129e7cfe89b8266371987a72a6c44d610b69
DmitrenkoNIkitaa <nikita.dmytrenko@upholding.biz>
2018-10-22 10:50:01 +0300

    Add additional configuration.

--- src/main/java/com/exrates/me/configuration/WebSecurityConfiguration.java
+++ src/main/java/com/exrates/me/configuration/WebSecurityConfiguration.java
@@ -33,21 +33,15 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) {
-        web.ignoring().antMatchers("/webjars/**","/resources/**","/swagger-resources/**","/api/**","/swagger-ui.html");
+        web.ignoring().antMatchers("/webjars/**","/resources/**");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/actuator").permitAll()
-                .antMatchers("/actuator/**").permitAll()
                 .antMatchers("/login","/logout.do").permitAll()
-                .antMatchers("/swagger-resources/**").permitAll()
-                .antMatchers("/swagger-ui.html").permitAll()
                 .antMatchers("/**").authenticated()
-                .antMatchers("/api/**").permitAll()
-                .antMatchers("/v2/**").permitAll()
                 .and()
                 .formLogin()
                 .loginProcessingUrl("/login.do")
